75d3096e1ce7b9272dfa5da0f76a1de1c8f0a019
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2022-12-26 10:28:45 +0000

    Deploying to gh-pages from @ Anton0694/goit-js-hw-09@81464e0fb9ec4ccc9dd2c901a18ed63c6af484ad 🚀

---
+++ 03-promises.ed27451f.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]'),document.querySelector('button[type="amount"]');
+//# sourceMappingURL=03-promises.ed27451f.js.map

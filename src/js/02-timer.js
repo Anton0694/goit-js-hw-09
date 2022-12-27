60b7cc42a069a76ce63eed78f5cb1575cb4b2be0
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2022-12-27 12:15:22 +0200

    delete comments

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,13 +3,11 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 const options = {
-  enableTime: true,  /* enableTime	Boolean	false	Enables time picker */
-  time_24hr: true, /* time_24hr	boolean	false	Displays time picker in 24 hour mode without AM/PM selection when enabled. */
-  defaultDate: new Date(), /* defaultDate	String	null	Sets the initial selected date(s).
-If you're using mode: "multiple" or a range calendar supply an Array of Date objects or an Array of date strings which follow your dateFormat.
-Otherwise, you can supply a single Date object or a date string. */
-  minuteIncrement: 1, /* minuteIncrement	Integer	5	Adjusts the step for the minute input (incl. scrolling) */
-  onClose(selectedDates) {  /* onClose	Function, [functions]	null	Function(s) to trigger on every time the calendar is closed. See Events API */
+  enableTime: true,  
+  time_24hr: true, 
+  defaultDate: new Date(),
+  minuteIncrement: 1, 
+  onClose(selectedDates) {  
     onInputDate(selectedDates[0]);
   },
 };
@@ -29,10 +27,9 @@ refs.start.setAttribute('disabled', 'disabled');
 let timeId = null;
 const INTERVAL = 1000;
 
-/** functions */
 function onInputDate(selectedDates) {
   if (selectedDates <= Date.now()) {
-    // alert('Please choose a date in the future');
+    
     Notiflix.Notify.failure('Please choose a date in the future');
   } else {
     refs.start.removeAttribute('disabled', 'disabled');
